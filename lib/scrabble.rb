de948811cd0c4978315b2b12a9547b1a9469660e
Andrew Lee & Jeff Lalic <student@epicodus.com>
2015-04-21 13:43:31 -0700

    One letter working

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,4 +1,12 @@
 class String
   define_method(:scrabble) do
+    letters = self.split("")
+    total_score = 0
+    letters.each() do |letter|
+      scrabble_key = { "e" => 1 }
+      score = scrabble_key.fetch(letter)
+      total_score += score
+    end
+    total_score
   end
 end
--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -2,7 +2,11 @@ require('rspec')
 require('scrabble.rb')
 
 describe('String#scrabble') do
-  it("it should split a string into an array of letters") do
+  it("should split a string into an array of letters") do
     expect(("epi").scrabble()).to(eq(["e", "p", "i"]))
+  end
+
+  it("should return a value for each letter") do
+    expect(("a").scrabble()).to(eq(1))
   end
 end
